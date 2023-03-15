be5be48111196873a5062c0349bac01e18d1a52d
ene-h <100203757+ene-h@users.noreply.github.com>
2023-03-15 11:36:48 +0100

    Update user_module_socd.v

--- src/user_module_socd.v
+++ src/user_module_socd.v
@@ -3,12 +3,12 @@ module socd(
 	output up_C, output down_C, output left_C, output right_C
 );
 	
-	always @(up||down)
-		up_C = up;
-		down_C = (up && down) ? 0 : down;
+  //always @(up or down)
+  assign up_C = up;
+  assign	down_C = (up && down) ? 0 : down;
 	
-	always @(left||right)
-		left_C = (left&&right) ? 0 : left;
-		right_C = (left&&right) ? 0 : right;
+  //always @(left or right)
+  assign left_C = (left && right) ? 0 : left;
+  assign right_C = (left && right) ? 0 : right;
 	
 endmodule
